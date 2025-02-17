4245899cd014152f24516c4dff204c078961dcae
Austin Gregg-Smith <blooop@gmail.com>
2025-02-17 15:52:12 +0000

    add isolated isaac42

---
+++ scripts/isolated_isaac42.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env -S uv run
+# /// script
+# dependencies = [
+#   "isaacsim==4.2.0.2",
+#   "isaacsim-extscache-kit==4.2.0.2",
+#   "isaacsim-extscache-kit-sdk==4.2.0.2",
+#   "isaacsim-extscache-physics==4.2.0.2",
+# ]
+# ///
+
+import os
+import subprocess
+
+os.environ["OMNI_KIT_ACCEPT_EULA"] = "yes"
+subprocess.run(["isaacsim", "omni.isaac.sim.kit"], check=True)
+
+# # raw commands
+# isaacsim omni.isaac.sim.kit
+# isaacsim omni.isaac.sim.python.kit
