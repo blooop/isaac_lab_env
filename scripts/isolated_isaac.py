88f1899eb3e1f72ef38d78c5d603430abcc31ddf
Austin Gregg-Smith <blooop@gmail.com>
2025-02-24 16:23:44 +0000

    add python version and url for isolated_isaac.py

--- scripts/isolated_isaac.py
+++ scripts/isolated_isaac.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env -S uv run
 # /// script
+# requires-python = "==3.10"
 # dependencies = [
 #   "isaacsim[all,extscache]==4.5.0"
 # ]
+# [[tool.uv.index]]
+# url = "https://pypi.nvidia.com"
 # ///
 
 import os
